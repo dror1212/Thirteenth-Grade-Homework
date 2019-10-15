155aaba1986bea4a1125afe5344566853e835aba
dror1212 <drortal12@gmail.com>
2019-10-15 16:12:56 +0300

    Add the functions docmentation

--- BullsAndCows/BullsAndCows/Main.c
+++ BullsAndCows/BullsAndCows/Main.c
@@ -4,6 +4,7 @@
 
 void main(void)
 {
+	// Variable definition
 	unsigned short players[TWO] = { 1234, 5679 };
 	unsigned short turn = ONE;
 	unsigned short guess;
@@ -11,23 +12,39 @@ void main(void)
 	unsigned short hits[FOUR] = { ZERO };
 	unsigned short amountOfHits = ZERO;
 	unsigned short amountOfKliaa;
+	enum BOOLEAN goOn = TRUE;
 
+	// Go until someone guess correctly
 	while (amountOfHits < FOUR)
 	{
+		// Empty the vector
 		EmptyAVector(hits, FOUR);
 
+		// Change to the other player turn
 		turn = !turn;
 
-		printf("Player %hu give a guess\n", turn + ONE);
-		scanf("%hu", &guess);
+		// Go untill the number is valid
+		while (goOn)
+		{
+			printf("Player %hu give a guess\n", turn + ONE);
+			scanf("%hu", &guess);
 
+			// Check if the number is valid (four digits long)
+			(NumOfDigits(guess) == FOUR) ? (goOn = FALSE) : (goOn = TRUE);
+		}
+		goOn = TRUE;
+
+		// Count what turn is it
 		turnsToWin += (!turn);
 
+		// Calculate the hits and kliaas
 		CheckHits(players[!turn], guess, hits);
 		CheckKliaa(players[!turn], guess, hits);
 
+		// Count the hits and kliaas
 		amountOfHits = SumOfDigits(hits, FOUR, ONE);
 		amountOfKliaa = SumOfDigits(hits, FOUR, TEN);
+
 		printf("Player %hu hit %hu times and kala %hu times\n", turn + ONE, amountOfHits, amountOfKliaa);
 	}
 
@@ -35,21 +52,55 @@ void main(void)
 	scanf("%hu", &guess);
 }
 
+//--------------------------------------------------------------------------------------------
+//											Check Hits
+//										------------------
+//
+// General		: The function gets the guess and the numbers, check if there are any hits.
+//
+// Parameters   :
+//			playerNumber - The offset of the other player
+//			guess - The guess of the player
+//			hits - The vector where the info is saved
+//
+// Return Value : None.
+//
+//--------------------------------------------------------------------------------------------
 void CheckHits(unsigned short playerNumber, unsigned short guess, unsigned short hits[])
 {
+	// Variable definition
 	unsigned short counter = ZERO;
+
+	// Check if there is a hit
 	for (; playerNumber + guess; playerNumber /= TEN, guess /= TEN, counter++)
 	{
 		hits[counter] = (playerNumber % TEN == guess % TEN) ? ONE : ZERO;
 	}
 }
 
+//--------------------------------------------------------------------------------------------
+//											Check Kliaa
+//										------------------
+//
+// General		: The function gets the guess and the numbers, check if there are any kliaas.
+//
+// Parameters   :
+//			playerNumber - The offset of the other player
+//			guess - The guess of the player
+//			hits - The vector where the info is saved
+//
+// Return Value : None.
+//
+//--------------------------------------------------------------------------------------------
+
 void CheckKliaa(unsigned short playerNumber, unsigned short guess, unsigned short hits[])
 {
+	// Variable definition
 	unsigned short counter;
 	unsigned short secondCounter;
 	unsigned short numberHolder;
 
+	// Check if there is a kliaa
 	for (counter = ZERO; guess; counter++, guess /= TEN)
 	{
 		for (numberHolder = playerNumber, secondCounter = ZERO; numberHolder; secondCounter++, numberHolder /= TEN)
